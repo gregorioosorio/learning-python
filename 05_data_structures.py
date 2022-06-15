1ae25af19d59b0a6937960ec8ab34e1ba5de05f3
Gregorio Osorio <gregorioosorio@Gregorios-MacBook-Pro.local>
2022-06-15 18:02:44 -0500

    data structures 2

--- 05_data_structures.py
+++ 05_data_structures.py
@@ -10,10 +10,11 @@ print(numbers2)
 
 print([(x, y) for x in [1,2,3] for y in [1,2,3] if x != y])
 
+# old fashion
 ret = []
 for x in [1,2,3]:
     for y in [1,2,3]:
         if x != y:
             ret.append((x,y))
-
 print(ret)
+
